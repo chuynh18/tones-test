f2077f691fae7a99d26e109d65bd8673f3a793e0
chuynh18 <38088640+chuynh18@users.noreply.github.com>
2019-07-24 22:24:08 -0700

    fix errors before a note is played

--- assets/js/keyboardtest.js
+++ assets/js/keyboardtest.js
@@ -22,20 +22,24 @@ window.addEventListener("load", function() {
 
       key.addEventListener("mouseup", function() {
          key.style.fill = key.dataset.fill;
-         gain.gain.linearRampToValueAtTime(0.000001, audio.currentTime + 0.5);
+         if (typeof note !== "undefined") {
+            gain.gain.linearRampToValueAtTime(0.000001, audio.currentTime + 0.5);
 
-         setTimeout(() => {
-            note.stop();
-         }, 500);  
+            setTimeout(() => {
+               note.stop();
+            }, 500);  
+         }
       });
 
       key.addEventListener("mouseout", function() {
          key.style.fill = key.dataset.fill;
-         gain.gain.linearRampToValueAtTime(0.000001, audio.currentTime + 0.5);
+         if (typeof note !== "undefined") {
+            gain.gain.linearRampToValueAtTime(0.000001, audio.currentTime + 0.5);
 
-         setTimeout(() => {
-            note.stop();
-         }, 500);  
+            setTimeout(() => {
+               note.stop();
+            }, 500);  
+         }
       });
    }
 
