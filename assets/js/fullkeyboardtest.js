e886094c7928b1f70e158934c23d20d1f9e4e1e6
chuynh18 <38088640+chuynh18@users.noreply.github.com>
2019-07-31 17:09:42 -0700

    working webaudio api version (at least in Chrome) with basic damper pedal.  damper pedal does not damp repeated notes when toggled off

--- assets/js/fullkeyboardtest.js
+++ assets/js/fullkeyboardtest.js
@@ -1,19 +1,34 @@
 "use strict";
 
-let state = {
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+const CONSTANTS = {
+   noteFade: 0.8
+}
+
+const state = {
+   audioContext: new AudioContext(),
    kb: undefined,
-   audio: []
+   audio: [],
+   pedal: false
 };
 
 window.addEventListener("load", function() {
    state.kb = document.getElementById("kb").getSVGDocument();
+
+   state.kb.addEventListener("click", function() {
+      state.audioContext.resume().then(() => {
+         console.log("AudioContext is now active.");
+      });
+   }, {once: true});
 
    const rects = state.kb.getElementsByTagName("rect");
 
    for (let i = 0; i < rects.length; i++) {
       const key = rects[i];
 
-      preload(`assets/audio/${Number(key.id)}.webm`, state.audio);
+      preload(`assets/audio/${Number(key.id)}.webm`, i);
+      console.log(`preloading assets/audio/${Number(key.id)}.webm`);
 
       key.addEventListener("mousedown", function() {
          startPlaying(i, key);
@@ -31,20 +46,72 @@ window.addEventListener("load", function() {
 
 function startPlaying(i, key) {
    key.style.fill = "red";
-   state.audio[i].play();
+
+   const note = state.audio[i];
+   
+   note.source = state.audioContext.createBufferSource();
+   note.source.buffer = note.buffer;
+   note.gain = state.audioContext.createGain();
+   note.source.connect(note.gain);
+   note.gain.connect(state.audioContext.destination);
+
+   note.source.start(0);
 }
 
 function stopPlaying(i, key) {
    key.style.fill = key.dataset.fill;
-   state.audio[i].pause();
-   state.audio[i].currentTime = 0;
+   const note = state.audio[i];
+   noteStop(note);
 }
 
-function preload(url, dest) {
-   const audio = new Audio();
-   audio.src = url;
+function noteStop(note) {
+   try {
+      if (note.source && !state.pedal) {
+         note.gain.gain.exponentialRampToValueAtTime(0.00001, state.audioContext.currentTime + CONSTANTS.noteFade);
+         note.source.stop(state.audioContext.currentTime + CONSTANTS.noteFade);
+      }
+   } catch (e) {
+      console.log(e);
+   }
+}
 
-   if (typeof dest !== "undefined") {
-      dest.push(audio);
+function preload(url, index) {
+   const req = new XMLHttpRequest;
+   req.open("GET", url, true);
+   req.responseType = 'arraybuffer';
+
+   const note = {
+      buffer: null,
+      source: null,
+      gain: null
+   };
+
+   req.onload = function() {
+      state.audioContext.decodeAudioData(req.response,
+         function(buffer) {
+            note.buffer = buffer;
+
+            state.audio[index] = note;
+         },
+         function(err) {
+            console.log(err);
+         });
+    }
+    req.send();
+}
+
+function togglePedal() {
+   const damperButton = document.getElementById("damper");
+
+   if (state.pedal) {
+      state.pedal = false;
+      damperButton.textContent = "Damper pedal OFF";
+
+      for (let i = 0; i < state.audio.length; i++) {
+         noteStop(state.audio[i]);
+      }      
+   } else {
+      state.pedal = true;
+      damperButton.textContent = "Damper pedal ON";
    }
 }
