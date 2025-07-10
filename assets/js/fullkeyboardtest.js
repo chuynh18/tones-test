058c376f4225725648dc39095bde7a07d2c58c83
Chris <lamcraft2003@yahoo.com>
2025-07-09 22:49:44 -0700

    shuffle stuff around, but not sure how much I get from it since my architecture is so bad because I didn't think anything through 👺

--- assets/js/fullkeyboardtest.js
+++ assets/js/fullkeyboardtest.js
@@ -1,4 +1,5 @@
-import { CONSTANTS, state, colors, keyReference } from "./piano/resources.js"
+import { state, keyReference } from "./piano/resources.js"
+import { startPlayer, startPlaying, stopPlaying } from "./piano/midiPlayer.js";
 
 window.addEventListener("load", function() {
    state.kb = document.getElementById("kb").getSVGDocument();
@@ -49,39 +50,6 @@ window.addEventListener("load", function() {
    document.getElementById("playMidi").addEventListener("click", playMidi);
 });
 
-function startPlaying(i, key, color = "red", gain = 1) {
-   key.style.fill = color;
-
-   const note = state.audio[i];
-   
-   note.source = state.audioContext.createBufferSource();
-   note.source.buffer = note.buffer;
-   note.gain = state.audioContext.createGain();
-   note.source.connect(note.gain);
-   note.gain.connect(state.audioContext.destination);
-
-   note.gain.gain.value = gain;
-
-   note.source.start(0);
-}
-
-function stopPlaying(i, key) {
-   key.style.fill = key.dataset.fill;
-   const note = state.audio[i];
-   noteStop(note);
-}
-
-function noteStop(note) {
-   try {
-      if (note.source && !state.pedal) {
-         note.gain.gain.exponentialRampToValueAtTime(0.05, state.audioContext.currentTime + CONSTANTS.noteFade);
-         note.source.stop(state.audioContext.currentTime + CONSTANTS.noteFade);
-      }
-   } catch (e) {
-      console.log(e);
-   }
-}
-
 function preload(url, index) {
    const req = new XMLHttpRequest;
    req.open("GET", url, true);
@@ -128,48 +96,4 @@ function togglePedal() {
 function playMidi() {
    state.midiIndex = Number(document.getElementById("midiIndex").value);
    startPlayer(Number(state.midiIndex));
-}
-
-function startPlayer(startIndex = 0) {
-   if (globalThis.midiFile) {
-      const ticksPerSecond = globalThis.midiFile.header.ticksPerSecond;
-      const playableTracks = globalThis.midiFile.tracks.filter(track => track.playableMusic);
-      document.getElementById("midiTotalLength").innerHTML = `${playableTracks[0].playableMusic.length}`;
-      playableTracks.forEach((track, trackNum) => {
-         let offset = 0;
-
-         if (startIndex > 0) {
-            offset = 1000 * track.playableMusic[startIndex].startTime / ticksPerSecond;
-         }
-
-         for (let i = startIndex; i < track.playableMusic.length; i++) {
-            const midiEvent = track.playableMusic[i];
-            if (midiEvent.pianoNote) {
-               const startMillis = (1000 * midiEvent.startTime / ticksPerSecond) - offset;
-
-               (function(i){setTimeout(function() {
-                  playNoteForDuration(
-                     midiEvent.pianoNote,
-                     1000 * midiEvent.duration / ticksPerSecond,
-                     colors[trackNum],
-                     midiEvent.velocity
-                  );
-                  state.midiIndex = i;
-                  document.getElementById("currentPosition").innerHTML = `Current note is ${state.midiIndex} out of `;
-               }, startMillis);})(i);
-            }
-         }
-      });
-
-   } else {
-      console.log("MIDI not loaded!");
-   }
-}
-
-function playNoteForDuration(pianoKeyNumber, duration, color, velocity) {
-   const volume = velocity/127;
-   startPlaying(keyReference[pianoKeyNumber], state.rects[keyReference[pianoKeyNumber]], color, volume);
-   setTimeout(function() {
-      stopPlaying(keyReference[pianoKeyNumber], state.rects[keyReference[pianoKeyNumber]])
-   }, duration);
 }
---
+++ assets/js/piano/midiPlayer.js
@@ -0,0 +1,83 @@
+import { CONSTANTS, colors, state, keyReference } from "./resources.js"
+
+export function startPlayer(startIndex = 0) {
+   if (globalThis.midiFile) {
+      const ticksPerSecond = globalThis.midiFile.header.ticksPerSecond;
+      const playableTracks = globalThis.midiFile.tracks.filter(track => track.playableMusic);
+      document.getElementById("midiTotalLength").innerHTML = `${playableTracks[0].playableMusic.length}`;
+      playableTracks.forEach((track, trackNum) => {
+         let offset = 0;
+
+         if (startIndex > 0) {
+            offset = 1000 * track.playableMusic[startIndex].startTime / ticksPerSecond;
+         }
+
+         for (let i = startIndex; i < track.playableMusic.length; i++) {
+            const midiEvent = track.playableMusic[i];
+            const startMillis = (1000 * midiEvent.startTime / ticksPerSecond) - offset;
+
+            processMidiEvent(midiEvent, startMillis, ticksPerSecond, trackNum, i);
+
+         }
+      });
+
+   } else {
+      console.log("MIDI not loaded!");
+   }
+}
+
+function processMidiEvent(midiEvent, startMillis, ticksPerSecond, trackNum, i) {
+   if (midiEvent.pianoNote) {
+      (function(i){setTimeout(function() {
+         playNoteForDuration(
+            midiEvent.pianoNote,
+            1000 * midiEvent.duration / ticksPerSecond,
+            colors[trackNum],
+            midiEvent.velocity
+         );
+         state.midiIndex = i;
+         document.getElementById("currentPosition").innerHTML = `Current note is ${state.midiIndex} out of `;
+      }, startMillis);})(i);
+   }
+}
+
+function playNoteForDuration(pianoKeyNumber, duration, color, velocity) {
+   const volume = velocity/127;
+   startPlaying(keyReference[pianoKeyNumber], state.rects[keyReference[pianoKeyNumber]], color, volume);
+   setTimeout(function() {
+      stopPlaying(keyReference[pianoKeyNumber], state.rects[keyReference[pianoKeyNumber]])
+   }, duration);
+}
+
+export function startPlaying(i, key, color = "red", gain = 1) {
+   key.style.fill = color;
+
+   const note = state.audio[i];
+   
+   note.source = state.audioContext.createBufferSource();
+   note.source.buffer = note.buffer;
+   note.gain = state.audioContext.createGain();
+   note.source.connect(note.gain);
+   note.gain.connect(state.audioContext.destination);
+
+   note.gain.gain.value = gain;
+
+   note.source.start(0);
+}
+
+export function stopPlaying(i, key) {
+   key.style.fill = key.dataset.fill;
+   const note = state.audio[i];
+   noteStop(note);
+}
+
+function noteStop(note) {
+   try {
+      if (note.source && !state.pedal) {
+         note.gain.gain.exponentialRampToValueAtTime(0.05, state.audioContext.currentTime + CONSTANTS.noteFade);
+         note.source.stop(state.audioContext.currentTime + CONSTANTS.noteFade);
+      }
+   } catch (e) {
+      console.log(e);
+   }
+}
